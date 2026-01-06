b5da294230e2ef478864901aed5ee0f8ad5a6190
natitedros <tedros.natnael@gmail.com>
2026-01-05 20:55:49 -0500

    added segmentation functionalities

--- backend/hed_model/hed_inference.py
+++ backend/hed_model/hed_inference.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 
 _net = None  # will be initialized on startup
-
 class CropLayer(object):
     def __init__(self, params, blobs):
         self.startX = self.startY = 0
@@ -68,3 +67,109 @@ def generate_hed_image(img: np.ndarray) -> np.ndarray:
     hed = (255 * hed).astype("uint8")
 
     return hed
+
+def generate_threshold_image(img: np.ndarray) -> np.ndarray:
+    """
+    Runs HED edge detection on a BGR OpenCV image.
+
+    Returns:
+        uint8 numpy array (H, W) with values [0, 255]
+    """
+    if img is None:
+        raise ValueError("Input image is None")
+
+    H, W = img.shape[:2]
+
+    blob = cv2.dnn.blobFromImage(
+        img,
+        scalefactor=0.7,
+        size=(W, H),
+        mean=(105, 117, 123),
+        swapRB=False,
+        crop=False,
+    )
+
+    _net.setInput(blob)
+    hed = _net.forward()
+
+    hed = hed[0, 0, :, :]
+    hed = (255 * hed).astype("uint8")
+    blur = cv2.GaussianBlur(hed, (3, 3), 0)
+    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+    return thresh
+
+def generate_colored_object_image(img: np.ndarray) -> np.ndarray:
+    """
+    Runs HED edge detection on a BGR OpenCV image.
+
+    Returns:
+        uint8 numpy array (H, W) with values [0, 255]
+    """
+    if img is None:
+        raise ValueError("Input image is None")
+
+    H, W = img.shape[:2]
+
+    blob = cv2.dnn.blobFromImage(
+        img,
+        scalefactor=0.7,
+        size=(W, H),
+        mean=(105, 117, 123),
+        swapRB=False,
+        crop=False,
+    )
+
+    _net.setInput(blob)
+    hed = _net.forward()
+
+    hed = hed[0, 0, :, :]
+    hed = (255 * hed).astype("uint8")
+    blur = cv2.GaussianBlur(hed, (3, 3), 0)
+    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    n_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(thresh, connectivity=4)
+
+    # Create false color image
+    colors = np.random.randint(0, 255, size=(n_labels, 3), dtype=np.uint8)
+    colors[0] = [0, 0, 0]
+    false_colors = colors[labels]
+
+    return false_colors
+
+def generate_colored_filtered_areas_image(img: np.ndarray) -> np.ndarray:
+    
+    if img is None:
+        raise ValueError("Input image is None")
+
+    H, W = img.shape[:2]
+
+    blob = cv2.dnn.blobFromImage(
+        img,
+        scalefactor=0.7,
+        size=(W, H),
+        mean=(105, 117, 123),
+        swapRB=False,
+        crop=False,
+    )
+
+    _net.setInput(blob)
+    hed = _net.forward()
+
+    hed = hed[0, 0, :, :]
+    hed = (255 * hed).astype("uint8")
+    blur = cv2.GaussianBlur(hed, (3, 3), 0)
+    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    n_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(thresh, connectivity=4)
+
+    # Create false color image
+    colors = np.random.randint(0, 255, size=(n_labels, 3), dtype=np.uint8)
+    colors[0] = [0, 0, 0]
+    false_colors_area_filtered = colors[labels]
+    MIN_AREA = 50
+    for i, centroid in enumerate(centroids[1:], start=1):
+        area = stats[i, 4]
+        if area > MIN_AREA:
+            cv2.drawMarker(false_colors_area_filtered, (int(centroid[0]), int(centroid[1])),
+                        color=(255, 255, 255), markerType=cv2.MARKER_CROSS)
+
+    return false_colors_area_filtered
